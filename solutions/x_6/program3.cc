d50138631dc265c98c2c5b4b1822389d98b486be
polykarpospolykarpidis <polykarpospolykarpidis@gmail.com>
2025-01-02 15:05:22 +0200

    program3.cc correct

--- solutions/x_6/program3.cc
+++ solutions/x_6/program3.cc
@@ -12,17 +12,17 @@
 int main(){
 
     const std::chrono::time_point<std::chrono::steady_clock> start = std::chrono::steady_clock::now();
-    
-    for(int i=0;i!=100;++i){
-        std::cout<<"Hello ";
+    size_t sum = 0;
+    for(int i=0;i!=1000000;++i){
+        sum += i;
     }
-    std::cout<<std::endl;
     
-    const std::chrono::time_point<std::chrono::steady_clock> end = std::chrono::steady_clock::now();
+    const auto end = std::chrono::steady_clock::now();
    
-    const auto d = std::chrono::duration_cast<std::chrono::milliseconds>(end-start) ;
- 
-    std::cout<< "time spand: "<< d << " milliseconds\n";
+    std::cout
+            << "time spand: "
+            <<  std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count()
+            <<  " milliseconds\n";
  
     return 0;
 }
